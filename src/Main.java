edd618c9125d06682fa9fa550f238cf6cadf5818
okunsimon3 <111754449+okunsimon3@users.noreply.github.com>
2024-02-05 22:36:12 -0500

    Completed

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int creditBalance = 5000;
+        double interestRate = 0.17;
+        double oneMonth = 0;
+        double twoMonth = 0;
+
+        oneMonth = creditBalance * interestRate;
+        twoMonth = (creditBalance + oneMonth) * interestRate;
+
+        System.out.println("Your interest for the first month is: " + oneMonth);
+        System.out.println("Your interest for the second month is: " + twoMonth);
+
+    }
+}
